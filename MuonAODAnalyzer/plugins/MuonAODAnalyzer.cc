5a1ca5f9341db22fd4664c33b9d71f7e5182ecdd
Nikolaos Plastiras <nipla31@gmail.com>
2025-06-05 12:55:17 +0200

    Changed the way branch dxy is filled

--- MuonAODAnalyzer/plugins/MuonAODAnalyzer.cc
+++ MuonAODAnalyzer/plugins/MuonAODAnalyzer.cc
@@ -198,6 +198,10 @@ void MuonAODAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& i
       muon_dxy.push_back( (&*muon)->innerTrack()->dxy(PV));
       muon_dz.push_back( (&*muon)->innerTrack()->dz(PV));
     }
+    else if(!((&*muon)->outerTrack()).isNull()){
+      muon_dxy.push_back( (&*muon)->outerTrack()->dxy(PV));
+      muon_dz.push_back( (&*muon)->outerTrack()->dz(PV));
+    }
     else{
       muon_dxy.push_back(-999.);
       muon_dz.push_back(-999.);
